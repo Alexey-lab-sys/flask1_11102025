4cb69788c69fd15d02f213cab1916f6431a343db
Alexey Trebunskiy <a.trebounskiy@gmail.com>
2025-10-17 19:48:54 +0300

    Done. M1:part1:task1-2 and update README.md

--- app.py
+++ app.py
@@ -53,5 +53,26 @@ def get_quotes():
     return jsonify(quotes)
 
 
+@app.get("/params/<value>")
+def get_params(value: str):
+    """ Пример динамического URL'a."""
+    return jsonify(param=value, value_type=str(type(value))), 200
+
+
+# /quotes/1
+# /quotes/2
+# /quotes/3
+# ...
+# /quotes/n-1
+# /quotes/n
+
+@app.get("/quotes/<int:quote_id>")
+def get_quote_by_id(quote_id: int):
+    """ Return quote by id from 'quotes' list."""
+    for quote in quotes:
+        if quote["id"] == quote_id:
+            return jsonify(quote), 200
+    return jsonify(error=f"Quote with id={quote_id} not found"), 404
+
 if __name__ == "__main__":
     app.run(debug=True)
